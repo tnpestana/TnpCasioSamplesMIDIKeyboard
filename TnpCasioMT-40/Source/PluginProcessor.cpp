cf325ab43fe350ba17dc4c484eaf1dfb4d3b0d4e
Tiago Pestana <tnevespestana@gmail.com>
2018-07-26 14:56:52 +0100

    save and retrieve app state between sessions

--- TnpCasioMT-40/Source/PluginProcessor.cpp
+++ TnpCasioMT-40/Source/PluginProcessor.cpp
@@ -270,15 +270,17 @@ AudioProcessorEditor* TnpCasioMt40AudioProcessor::createEditor()
 //==============================================================================
 void TnpCasioMt40AudioProcessor::getStateInformation (MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+	auto state = treeState.copyState();
+	std::unique_ptr<XmlElement> xml(state.createXml());
+	copyXmlToBinary(*xml, destData);
 }
 
 void TnpCasioMt40AudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+	std::unique_ptr<XmlElement> xmlState(getXmlFromBinary(data, sizeInBytes));
+	if (xmlState.get() != nullptr)
+		if (xmlState->hasTagName(treeState.state.getType()))
+			treeState.replaceState(ValueTree::fromXml(*xmlState));
 }
 
 //==============================================================================
